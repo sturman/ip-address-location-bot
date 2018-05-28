52288e76dab93fbfca3aae77adad4a30735029cb
Roman Isko <iskoroman@gmail.com>
2018-05-28 17:41:48 +0300

    check if IP address with module 'ip-regex'

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 const Telegraf = require('telegraf')
-
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const rp = require('request-promise')
+const ipRegex = require('ip-regex')
 
 bot.command('start', (ctx) => {
   console.log('start', ctx.from)
   return ctx.reply('Welcome!')
 })
 
-bot.hears(/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/, (ctx) => {
-  let ip = ctx.match[0]
-
-  // console.log(ip)
-  function prepareRequestOptions (ipAddress) {
-    return {
-      url: 'https://ipvigilante.com/' + ipAddress + '/full',
+bot.on('text', (ctx) => {
+  let messageText = ctx.message.text
+  if (ipRegex({exact: true}).test(messageText)) {
+    let requestOptions = {
+      url: 'https://ipvigilante.com/' + messageText + '/full',
       json: true
     }
+    rp(requestOptions)
+      .then(function (response) {
+        console.log(response)
+        let data = response.data
+        if (response.status === 'success') {
+          return ctx.reply('ipv4: ' + data.ipv4 + '\n' +
+            'hostname: ' + data.hostname + '\n' +
+            'continent: ' + data.continent_name + '\n' +
+            'country: ' + data.country_name + '\n' +
+            'city: ' + data.city_name)
+        }
+      }).catch(function (err) {
+      return ctx.replyWithMarkdown('*Something wrong...*\nError details:\n' + JSON.stringify(err))
+    })
   }
-
-  rp.get(prepareRequestOptions(ip))
-    .then(function (res) {
-      console.log(res)
-      let data = res.data
-      if (res.status === 'success') {
-        return ctx.reply('ipv4: ' + data.ipv4 + '\n' +
-          'hostname: ' + data.hostname + '\n' +
-          'continent: ' + data.continent_name + '\n' +
-          'country: ' + data.country_name + '\n' +
-          'city: ' + data.city_name)
-      }
-    }).catch(function (err) {
-    return ctx.reply('Error\n' + JSON.stringify(err))
-  })
+  else {
+    return ctx.reply(`${ctx.message.text} is not an IP address`)
+  }
 })
 
 bot.startPolling()
