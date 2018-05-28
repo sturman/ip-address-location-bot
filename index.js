3799165f8a053cf0323688a50222eb38ec888e3e
Roman Isko <iskoroman@gmail.com>
2018-05-28 17:16:13 +0300

    apply standard.js code style

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-const Telegraf = require('telegraf');
+const Telegraf = require('telegraf')
 
-const app = new Telegraf(process.env.BOT_TOKEN);
-const rp  = require('request-promise');
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const rp = require('request-promise')
 
-app.command('start', (ctx) => {
-    console.log('start', ctx.from);
-    return ctx.reply('Welcome!')
+bot.command('start', (ctx) => {
+  console.log('start', ctx.from)
+  return ctx.reply('Welcome!')
 })
 
-app.hears(/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/, (ctx) => {
-    let ip = ctx.match[0];
-    // console.log(ip)
-    function prepareRequestOptions(ipAddress) {
-        return {
-            url : "https://ipvigilante.com/" + ipAddress + "/full",
-            json: true
-        };
+bot.hears(/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/, (ctx) => {
+  let ip = ctx.match[0]
+
+  // console.log(ip)
+  function prepareRequestOptions (ipAddress) {
+    return {
+      url: 'https://ipvigilante.com/' + ipAddress + '/full',
+      json: true
     }
+  }
 
-    rp.get(prepareRequestOptions(ip))
-        .then(function (res) {
-            console.log(res);
-            let data = res.data;
-            if (res.status === 'success') {
-                return ctx.reply('ipv4: ' + data.ipv4 + '\n' +
-                    'hostname: ' + data.hostname + '\n' +
-                    'continent: ' + data.continent_name + '\n' +
-                    'country: ' + data.country_name + '\n' +
-                    'city: ' + data.city_name)
-            }
-        }).catch(function (err) {
-        return ctx.reply("Error\n" + JSON.stringify(err))
-    })
-});
+  rp.get(prepareRequestOptions(ip))
+    .then(function (res) {
+      console.log(res)
+      let data = res.data
+      if (res.status === 'success') {
+        return ctx.reply('ipv4: ' + data.ipv4 + '\n' +
+          'hostname: ' + data.hostname + '\n' +
+          'continent: ' + data.continent_name + '\n' +
+          'country: ' + data.country_name + '\n' +
+          'city: ' + data.city_name)
+      }
+    }).catch(function (err) {
+    return ctx.reply('Error\n' + JSON.stringify(err))
+  })
+})
 
-app.startPolling()
+bot.startPolling()
